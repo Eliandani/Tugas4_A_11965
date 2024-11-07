3a66ff965fb4bd57acc19666a907f54163572614
Eliandani Andreskia Setiawan <220711965@students.uajy.ac.id>
2024-11-07 15:50:56 +0700

    Update Tugas_Modul4_A_Eldan_11965.py

--- Tugas_Modul4_A_Eldan_11965.py
+++ Tugas_Modul4_A_Eldan_11965.py
@@ -46,11 +46,10 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - Eldan</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\Documents Eliandani\Tugas\Semester 5\Machine Learning\Tugas4_A_11965'
     model_path = {
-        "AGG_model" : os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model" : os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model" : os.path.join(model_directory, r'DBSCAN_model.pkl'),
+        "AGG_model" : r'AGG_model.pkl',
+        "KMeans_model" : r'KMeans_model.pkl',
+        "DBSCAN_model" : r'DBSCAN_model.pkl',
     }
 
 models = {}
